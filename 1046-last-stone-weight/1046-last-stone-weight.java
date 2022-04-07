5ce265e0a1db3b0de22e264837f8a73fcee417a9
Harshit Sharma <iharshitsharma@outlook.com>
2022-04-08 01:03:49 +0530

    Time: 1 ms (98.64%), Space: 41.7 MB (47.75%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        Arrays.sort(stones);
+        
+        for(int i = stones.length - 1; i > 0; i--){
+            stones[i - 1] = stones[i] - stones[i - 1];
+            Arrays.sort(stones);
+        }
+        return stones[0];
+    }
+}
