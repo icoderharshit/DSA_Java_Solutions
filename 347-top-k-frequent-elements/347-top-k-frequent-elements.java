a4ff5aed5cde3ae0824b6b16bbaee806fa44afea
Harshit Sharma <iharshitsharma@outlook.com>
2022-04-10 01:44:19 +0530

    Time: 24 ms (22.09%), Space: 50.2 MB (65.94%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -4,6 +4,7 @@ class Solution {
         for(int num:nums){
             hm.put(num,hm.getOrDefault(num,0)+1);
         }
+        /*
         ArrayList<Integer> ar=new ArrayList<>();
         
         hm.entrySet().stream()
@@ -16,6 +17,18 @@ class Solution {
             result[i++]=num;
             else
                 break;
+        }
+        return result;
+        */
+        // Optimised Approach using Heap
+        PriorityQueue<Integer> heap = new PriorityQueue<>((n1, n2) -> hm.get(n1) - hm.get(n2));
+        for (int num: hm.keySet()) {
+          heap.add(num);
+          if (heap.size() > k) heap.poll();    
+        }
+        int[] result = new int[k];
+        for(int i = k - 1; i >= 0; --i) {
+            result[i] = heap.poll();
         }
         return result;
     }
