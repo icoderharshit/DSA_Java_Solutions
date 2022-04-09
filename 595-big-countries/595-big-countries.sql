ce4d8428ab3b555dc16c800301d87567a25594af
Harshit Sharma <iharshitsharma@outlook.com>
2022-04-10 01:55:40 +0530

    Time: 324 ms (36.40%), Space: 0B (100.00%) - LeetHub

--- 595-big-countries/595-big-countries.sql
+++ 595-big-countries/595-big-countries.sql
@@ -1,7 +1,9 @@
 # Write your MySQL query statement below
 # Below is mine written first query here in SQL on LeetCode :)
-# Select name,population,area from World WHERE area>=3000000 OR population>=25000000;
+Select name,population,area from World WHERE area>=3000000 OR population>=25000000;
 
 # trying leetcode approach for faster results
-SELECT name, population, area FROM world WHERE area >= 3000000 UNION
-SELECT name, population, area FROM world WHERE population >= 25000000;
+# SELECT name, population, area FROM world WHERE area >= 3000000 UNION
+# SELECT name, population, area FROM world WHERE population >= 25000000;
+
+# leetcode runtime analysis literally suck bruh... :/
