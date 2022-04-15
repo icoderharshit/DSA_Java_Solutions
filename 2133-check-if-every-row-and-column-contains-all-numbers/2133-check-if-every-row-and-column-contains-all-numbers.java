9b6a2562de7f95ff879311fc89c2f9801513ddf4
Harshit Sharma <iharshitsharma@outlook.com>
2022-04-15 09:14:41 +0530

    Time: 25 ms (67.49%), Space: 43.3 MB (87.67%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -5,13 +5,15 @@ class Solution {
             Set<Integer> row=new HashSet<>();
             Set<Integer> col=new HashSet<>();
             for(int j=0;j<n;j++){
-                if(row.contains(matrix[i][j]) || col.contains(matrix[j][i]))
+                // if(row.contains(matrix[i][j]) || col.contains(matrix[j][i]))
+                //     return false;
+                // else
+                // {
+                //     row.add(matrix[i][j]);
+                //     col.add(matrix[j][i]);
+                // }
+                if(!row.add(matrix[i][j]) || !col.add(matrix[j][i]))
                     return false;
-                else
-                {
-                    row.add(matrix[i][j]);
-                    col.add(matrix[j][i]);
-                }
             }
         }
         return true;
