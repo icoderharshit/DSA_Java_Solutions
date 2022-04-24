8066bcf774b4a48f1a36da47844a07ca944c87e6
Harshit Sharma <iharshitsharma@outlook.com>
2022-04-25 01:01:57 +0530

    Time: 217 ms (16.18%), Space: 102 MB (43.34%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -0,0 +1,35 @@
+class UndergroundSystem {
+    Map<Integer, Pair<String, Integer>> checkInMap = new HashMap<>();  
+    Map<String, Pair<Double, Integer>> routeMap = new HashMap<>(); 
+
+    public UndergroundSystem() {}
+
+    public void checkIn(int id, String stationName, int t) {
+        checkInMap.put(id, new Pair<>(stationName, t));
+    }
+
+    public void checkOut(int id, String stationName, int t) {
+        Pair<String, Integer> checkIn = checkInMap.get(id);
+        checkInMap.remove(id);
+
+        String routeName = checkIn.getKey() + "_" + stationName;
+        int totalTime = t - checkIn.getValue();
+
+        Pair<Double, Integer> route = routeMap.getOrDefault(routeName, new Pair<>(0.0, 0));
+        routeMap.put(routeName, new Pair<>(route.getKey() + totalTime, route.getValue() + 1));
+    }
+
+    public double getAverageTime(String startStation, String endStation) {
+        String routeName = startStation + "_" + endStation;
+        Pair<Double, Integer> trip = routeMap.get(routeName);
+        return trip.getKey() / trip.getValue();
+    }
+}
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem obj = new UndergroundSystem();
+ * obj.checkIn(id,stationName,t);
+ * obj.checkOut(id,stationName,t);
+ * double param_3 = obj.getAverageTime(startStation,endStation);
+ */
